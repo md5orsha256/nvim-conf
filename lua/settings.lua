fff013d74e5eeb7afff217b82262ca3f13b11d6e
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2023-05-01 19:49:39 +0600

    simple chores

--- lua/settings.lua
+++ lua/settings.lua
@@ -53,6 +53,7 @@ require('nvim-tree').setup {  -- SIDEBAR NAVIGATION CONFIG
     },
   },
   renderer = {
+    highlight_opened_files = "name",
     indent_markers = {
       enable = true,
       icons = {
@@ -139,20 +140,20 @@ require('nvim-tree').setup {  -- SIDEBAR NAVIGATION CONFIG
 require("bufferline").setup {  -- TABS CONFIG
     options = {
       indicator = {
-        icon = "▎"
+        icon = "▎|>"
       },
       modified_icon = "●",
-      buffer_close_icon = "",
-      close_icon = "",
-      left_trunc_marker = "... ",
-      right_trunc_marker = " ...",
+      buffer_close_icon = "×",
+      close_icon = "",
+      left_trunc_marker = '',
+      right_trunc_marker = '',
       numbers = "ordinal",
       max_name_length = 15,
       max_prefix_length = 6,
       diagnostics = "nvim_lsp",
       show_buffer_icons = true,
-      show_buffer_close_icons = false,
-      show_close_icon = false,
+      show_buffer_close_icons = true,
+      show_close_icon = true,
       persist_buffer_sort = true,
       enforce_regular_tabs = true,
       diagnostics_indicator = function(count, level)
